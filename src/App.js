5451e313757c2aa51a02a9da888e5ead9da8bc0e
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-28 18:29:41 +0100

    Create SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="The chosen" animal="Lion" breed="White Lion" />
-      <Pet name="Dolphy" animal="Dolphin" breed="Amazon River Dolphin" />
-      <Pet name="Siso" animal="Husky" breed="Siberian Husky" />
+      <SearchParams />
     </div>
   );
 };
