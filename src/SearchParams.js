9396408e64883b54fe573c42107e577cba7ebde9
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-28 19:36:40 +0100

    Map data to component searchparams by using useState hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,64 @@
+import { useState } from "react";
+
+const ANIMALS = ["birds", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const location = "London, UK";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value), setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value), setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option /> {/* Nothing selected at first */}
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
