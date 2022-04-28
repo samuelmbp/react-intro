0be8d2dae546bfdcb7378ca363063b194f95a92d
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-28 14:43:58 +0100

    Add configuration for prettier formatter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 const Pet = (props) => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, props.name),
-		React.createElement('h2', {}, props.animal),
-		React.createElement('h2', {}, props.breed),
-	]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
 };
 
 const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, {
-			name: 'The Chosen',
-			animal: 'Lion',
-			breed: 'White Lion',
-		}),
-		React.createElement(Pet, {
-			name: 'Doplhy',
-			animal: 'Dolphin',
-			breed: 'Amazon River Dolphin',
-		}),
-		React.createElement(Pet, {
-			name: 'Siso',
-			animal: 'Husky',
-			breed: 'Siberian Husky',
-		}),
-	]);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "The Chosen",
+      animal: "Lion",
+      breed: "White Lion",
+    }),
+    React.createElement(Pet, {
+      name: "Doplhy",
+      animal: "Dolphin",
+      breed: "Amazon River Dolphin",
+    }),
+    React.createElement(Pet, {
+      name: "Siso",
+      animal: "Husky",
+      breed: "Siberian Husky",
+    }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
