d3315bc6f54cb3f418e6279c33e6bcfb3b9246f2
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-01 18:04:34 +0100

    Create carousel component to show thumbnail photos

--- src/Details.js
+++ src/Details.js
@@ -1,18 +1,24 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 class Details extends Component {
-  constructor(props) {
-    super(props);
+  /** Class constructor 
+    constructor(props) {
+      super(props);
 
-    this.state = { loading: true };
-  }
+      this.state = { loading: true };
+    }
+  */
+
+  /** Class property (new) */
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id${this.props.params.id}`
     );
     const json = await res.json();
-    
+
     this.setState({ loading: false, ...json.pets[0] });
   }
 
@@ -20,16 +26,20 @@ class Details extends Component {
     if (this.state.loading) return <h2>Loading...</h2>;
 
     // Destructing
-    const { animal, breed, city, state, description, name } = this.state; // e.g this.state.breed
+    const { animal, breed, city, state, description, name, images } =
+      this.state; // e.g this.state.breed
 
     return (
       <div className="details">
-        <h1>{name}</h1>
-        <h2>
-          {animal} - {breed} - {city} - {state}
-        </h2>
-        <button>Adopt {name}</button>
-        <p>{description}</p>
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city} - {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
       </div>
     );
   }
