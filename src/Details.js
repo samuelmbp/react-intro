b032fceeb5a6ad400bebb15eb16742d15920d487
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-01 17:22:34 +0100

    Fetch data in details component with componentDidMount method

--- src/Details.js
+++ src/Details.js
@@ -7,6 +7,15 @@ class Details extends Component {
     this.state = { loading: true };
   }
 
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id${this.props.params.id}`
+    );
+    const json = await res.json();
+    
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
   render() {
     if (this.state.loading) return <h2>Loading...</h2>;
 
@@ -25,4 +34,9 @@ class Details extends Component {
     );
   }
 }
-export default Details;
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+export default WrappedDetails;
