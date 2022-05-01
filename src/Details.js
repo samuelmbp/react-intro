fc09e5846621cd6a420e60b07ddfd5ef749e0577
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-01 16:55:04 +0100

    Make the details function component a class component

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,28 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
+class Details extends Component {
+  constructor(props) {
+    super(props);
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
-};
+    this.state = { loading: true };
+  }
 
+  render() {
+    if (this.state.loading) return <h2>Loading...</h2>;
+
+    // Destructing
+    const { animal, breed, city, state, description, name } = this.state; // e.g this.state.breed
+
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city} - {state}
+        </h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
 export default Details;
